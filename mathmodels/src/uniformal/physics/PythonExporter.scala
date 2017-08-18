3406f6755096f8a1768152d7ea63a3abc1c2cf97
Student-A <ai.aleryani@jacobs-university.de>
2017-08-18 09:06:21 +0200

    updates python exporter after review

--- mathmodels/src/uniformal/physics/PythonExporter.scala
+++ mathmodels/src/uniformal/physics/PythonExporter.scala
@@ -5,7 +5,7 @@ import documents._
 import modules._
 import symbols._
 import archives._
-
+import utils._
 import objects._
 
 import info.mathhub.lf.MitM.Foundation._
@@ -18,6 +18,8 @@ import Units.QEBase._
  */
 class PythonExporter extends Exporter {
   
+  override val outExt = "py"
+  
   val key = "mpd-python"
   private lazy val mpdtool = {
     val m = new MPDTool
@@ -25,90 +27,80 @@ class PythonExporter extends Exporter {
     m
   }
   
-  private[this] def pyIndent(n: Int): String = "\t"*n
+  private def pyIndent(n: Int): String = "\t"*n
   
-  private[this] def ensureIdentifierString(s :String): String = {
+  private def ensureIdentifierString(s :String): String = {
     var out: String = s
     if (s.headOption.getOrElse(return s).isDigit)
       out = "_"+out
     out
   }
   
-  private[this] class QuantityPyExpression(quantity: Quantity) {
+  private class QuantityPyExpression(state: String, quantity: Quantity) {
     var parameters: List[String] = Nil
     
-    val expression = makeQuantityPyExpression(quantity)
+    val expression = makeQuantityPyExpression(quantity.value)
     
-    private def makeQuantityPyExpression(q : Quantity): String = q match { 
-      case Quantity(QEMul(d1, d2, v1, v2), ft) => 
-        s"(${makeQuantityPyExpression(Quantity(v1, ft))} * ${makeQuantityPyExpression(Quantity(v2, ft))})" 
-      case Quantity(QEDiv(d1, d2, v1, v2), ft) => 
-        s"(${makeQuantityPyExpression(Quantity(v1, ft))} / ${makeQuantityPyExpression(Quantity(v2, ft))})" 
-      case Quantity(QEAdd(d, v1, v2), ft) => 
-        s"(${makeQuantityPyExpression(Quantity(v1, ft))} + ${makeQuantityPyExpression(Quantity(v2, ft))})" 
-      case Quantity(QESubtract(d, v1, v2), ft) => 
-        s"(${makeQuantityPyExpression(Quantity(v1, ft))} - ${makeQuantityPyExpression(Quantity(v2, ft))})" 
-      case Quantity(OMS(path), ft) => {
+    private def makeQuantityPyExpression(q : Term): String = q match {
+      case QEMul(d1, d2, v1, v2) => 
+        s"(${makeQuantityPyExpression(v1)} * ${makeQuantityPyExpression(v2)})" 
+      case QEDiv(d1, d2, v1, v2) => 
+        s"(${makeQuantityPyExpression(v1)} / ${makeQuantityPyExpression(v2)})" 
+      case QEAdd(d, v1, v2) => 
+        s"(${makeQuantityPyExpression(v1)} + ${makeQuantityPyExpression(v2)})" 
+      case QESubtract(d, v1, v2) => 
+        s"(${makeQuantityPyExpression(v1)} - ${makeQuantityPyExpression(v2)})" 
+      case OMS(path) => {
           parameters ::= path.name.toString
-          ensureIdentifierString(path.name.toString) 
+          state + "['" + ensureIdentifierString(path.name.toString) + "']" 
       }
-      case Quantity(OMA(OMID(path), x), ft) =>
-        throw new scala.MatchError("Undefined operation: " + path.toString())
-      case _ => throw new scala.MatchError("Match Error hodgepodge", q)
+      case l: OMLITTrait => l.toString
+      case OMV(n) => n.toPath
+      case OMA(OMID(path), _) =>
+        throw LocalError("Undefined operation: " + path.toString())
+      case t => throw LocalError("Match Error:" + t.toStr(true))
     }
     
   }
   
-  private[this] def makeRulePyLambda(rule: Rule): (String, String) = {
-    val expr = new QuantityPyExpression(rule.solution)
+  private[this] def makeRulePyLambda(state: String, rule: Rule): (String, String) = {
+    val expr = new QuantityPyExpression(state, rule.solution)
     (rule.solved.name.toString(), 
-        s"lambda (${expr.parameters.tail.foldRight(expr.parameters.head.toString()){(x, y) => x + "," + y}}): ${expr.expression}")
-        
+        s"lambda $state: ${expr.expression}")
+        //${expr.parameters.tail.foldRight(expr.parameters.head.toString()){(x, y) => x + "," + y}}
   }
   
   private[this] def quantityDeclsPyAttributes(mpd: MPD) = 
     mpd.quantityDecls.map(q => {Map(
         "name" -> s"'${q.name.toString}'",
         "parent" -> s"'${q.parent.toString}'",
-        "path" -> s"'${q.path.toString}'",
         "dimension" -> (q.dim match {
           case OMS(p) => s"'${p.name.toString}'"
           case _ => "''"
-          })
+          })//,
+         //q.df.toList.map{d => "compute" -> ???} :_*
         )
     })
     
   private[this] def lawsPyAttributes(mpd: MPD) = 
     mpd.laws.map(l => {Map(
         "name" -> s"'${l.name.toString}'",
-        "parent" -> s"'${l.parent.toString}'",
-        "path" -> s"'${l.path.toString}'") ++ 
-        l.rules.map(makeRulePyLambda(_))
+        "parent" -> s"'${l.parent.toString}'") ++ 
+        l.rules.map(makeRulePyLambda("state", _))
     })
   
   def pyObjectAssignment(lhs: String, objname: String, attrs: Map[String, String], currentIndentLevel: Int): String = {
-    s"""$lhs = type('$objname', (object,) , {
-${pyIndent(currentIndentLevel+1)}${attrs.map(kp => s"'${kp._1}': ${kp._2}").mkString(",\n"+pyIndent(currentIndentLevel+1))} 
-${pyIndent(currentIndentLevel+1)}})()"""
+    s"""$lhs = $objname(
+${pyIndent(currentIndentLevel+1)}${attrs.map(kp => s"${kp._1}= ${kp._2}").mkString(",\n"+pyIndent(currentIndentLevel+1))} 
+${pyIndent(currentIndentLevel)})"""
   }
     
   def constructPyFromMPD(mpdOpt: Option[MPD]) : Option[String] = {
     val mpd = mpdOpt.getOrElse(return None)
     val py = s"""
-class MPD_${mpd.name.toString()}
-${pyIndent(1)}laws = {}
-${pyIndent(1)}quantity_decls = {}
+from mpdbase import *
 
-${pyIndent(1)}def __init__(self):
-${pyIndent(2)}self.init_quantity_decls()
-${pyIndent(2)}self.init_laws()
-
-${pyIndent(1)}def __getitem__(self, quantity_decl_key):
-${pyIndent(2)}return self.quantity_decls[quantity_decl_key]
-
-${pyIndent(1)}def __call__(self, law_key):
-${pyIndent(2)}return self.laws[law_key]
-
+class MPD_${mpd.name.toString()}(MPDBase):
 ${pyIndent(1)}def init_quantity_decls(self):
 ${pyIndent(2)}${quantityDeclsPyAttributes(mpd).map{
   mp => pyObjectAssignment(s"self.quantity_decls[${mp("name")}]", "QuantityDecl", mp, 2)}.mkString("\n\n"+pyIndent(2))
@@ -121,7 +113,8 @@ ${pyIndent(2)}${lawsPyAttributes(mpd).map{
 
 
 """
-    Some(py)
+/**/
+Some(py)
   }
   
   def exportTheory(thy: DeclaredTheory, bf: BuildTask) {
